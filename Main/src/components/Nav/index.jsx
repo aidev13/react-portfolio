1c1addb62ef83bd77c073815c85cff7f188283c3
David Liebherr <ambition.aidev@gmail.com>
2023-10-24 13:00:09 -0500

    WIP, took out Link and added NavLink for active link style. addded view points for pages

--- Main/src/components/Nav/index.jsx
+++ Main/src/components/Nav/index.jsx
@@ -1,8 +1,18 @@
 
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './nav.css'
 
+
+
+
+
 const Nav = () => {
+
+  // const activeStyle = () => {({ isActive }) => ({
+  //   color: isActive ? '#fff' : '#545e6f',
+  //   background: isActive ? '#7600dc' : '#f0f0f0',
+  // })}
+
   return (
     <>
       <nav>
@@ -12,11 +22,21 @@ const Nav = () => {
         </div>
 
         <div>
-          <Link to="/"> Home </Link>
-          <Link to="/about"> About Me </Link>
-          <Link to="/portfolio"> Portfolio </Link>
-          <Link to="/contact"> Contact </Link>
-          <Link to="/resume"> Resume </Link>
+          <NavLink to="/" style={({ isActive }) => ({
+            color: isActive ? '#000' : 'rgb(143, 143, 143)',
+          })}> Home </NavLink>
+          <NavLink to="/about" style={({ isActive }) => ({
+            color: isActive ? '#000' : 'rgb(143, 143, 143)',
+          })}> About Me </NavLink>
+          <NavLink to="/portfolio" style={({ isActive }) => ({
+            color: isActive ? '#000' : 'rgb(143, 143, 143)',
+          })}> Portfolio </NavLink>
+          <NavLink to="/contact" style={({ isActive }) => ({
+            color: isActive ? '#000' : 'rgb(143, 143, 143)',
+          })}> Contact </NavLink>
+          <NavLink to="/resume" style={({ isActive }) => ({
+            color: isActive ? '#000' : 'rgb(143, 143, 143)',
+          })}> Resume </NavLink>
         </div>
 
       </nav>
--- Main/src/components/Page/Portfolio.jsx
+++ Main/src/components/Page/Portfolio.jsx
@@ -2,9 +2,56 @@
 import Header from "../Header";
 
 const Portfolio = () => {
-    return(
+    return (
         <>
-        <Header title='Portfolio' />
+            <Header title='Portfolio' />
+            <div className="viewPoint">
+                <div className="m-5 d-flex justify-content-center gap-5 ">
+                    <div className="card" style={{ width: '18rem' }}>
+                        <img src="..." className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">
+                                Some quick example text to build on the card title and make up the
+                                bulk of the card's content.
+                            </p>
+                            <a href="#" className="btn btn-primary">
+                                Go somewhere
+                            </a>
+                        </div>
+                    </div>
+
+                    <div className="card" style={{ width: '18rem' }}>
+                        <img src="..." className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">
+                                Some quick example text to build on the card title and make up the
+                                bulk of the card's content.
+                            </p>
+                            <a href="#" className="btn btn-primary">
+                                Go somewhere
+                            </a>
+                        </div>
+                    </div>
+
+                    <div className="card" style={{ width: '18rem' }}>
+                        <img src="..." className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">
+                                Some quick example text to build on the card title and make up the
+                                bulk of the card's content.
+                            </p>
+                            <a href="#" className="btn btn-primary">
+                                Go somewhere
+                            </a>
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
         </>
     )
 }
