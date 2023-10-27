addf7f62dde04dccee9798cf1b652ab056ed0f9a
David Liebherr <ambition.aidev@gmail.com>
2023-10-27 12:37:00 -0500

    WIP, updated the navbar

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -11,17 +11,20 @@ const activeStyle = ({ isActive }) => ({
 const Nav = () => {
   return (
     <>
-      <nav className="navbar   gap-3  sticky-top">
+      <nav className="navbar gap-3 sticky-top">
         <div className="container-fluid">
+
+          <button className="navbar-toggler me-4" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
           <div>
             <Link to="/" className="navbar-brand" id="logo">
               <img src="./ode.png" width="100px" alt="Code Dave Logo" />
             </Link>
           </div>
-          <button className="navbar-toggler me-4" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse pt-2 pb-2" id="navbarNavAltMarkup">
+
+
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div className="navbar-nav pt-2 me-4 d-flex justify-content-end flex-row">
               <NavLink to="/" style={activeStyle}> Home </NavLink>
               <NavLink to="/about" style={activeStyle}> About Me </NavLink>
@@ -30,6 +33,7 @@ const Nav = () => {
               <NavLink to="https://www.linkedin.com/in/david-liebherr-489a1951"> Resume </NavLink>
             </div>
           </div>
+          
         </div>
       </nav>
 
