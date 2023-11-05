ac6201e47ce17dd4a26d39d8b30d74843c299b5f
David Liebherr <ambition.aidev@gmail.com>
2023-11-04 21:02:41 -0500

    WIP, trail expreimental contact page...

--- src/page/Contact.jsx
+++ src/page/Contact.jsx
@@ -13,16 +13,19 @@ const Contact = () => {
           <div className='contact_header'>
             <img src="./snow-mountains.png" className='background' />
             <img src="./ode.png" className='foreground' />
-            <h1 className='contact_title'>//coNtact me</h1>
+              <div className='contact_title'>
+              <h2 className='mx-3'> //inFormation </h2>
+                      <section>
+                        <ul>
+                          <li>// phOne: 262/409/3767</li>
+                          <li>// eMail: ambition.aidev@gmail.com</li>
+                        </ul>
+                      </section>
+              </div>
           </div>
         </div>
-        <h2 className='mx-3'> //inFormation </h2>
-        <section>
-          <ul>
-            <li>// phOne: 262/409/3767</li>
-            <li>// eMail: ambition.aidev@gmail.com</li>
-          </ul>
-        </section>
+        
+
       </main>
     </>
   )
