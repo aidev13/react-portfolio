f782b080a30d76ead605ecaf08afd64386a34b56
David Liebherr <ambition.aidev@gmail.com>
2023-11-04 21:36:10 -0500

    WIP, updated resume and started form componrnt

--- src/page/Resume.jsx
+++ src/page/Resume.jsx
@@ -5,6 +5,9 @@ const Resume = () => {
   return (
     <>
         <Header>//reSume</Header>
+        <div className="text-center">
+          <tt>Please check out my LinkIn Page for complete review</tt>
+        </div>
     </>
   )
 }
