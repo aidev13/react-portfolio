671ba8b525cb1a52c040eb74ba4a42a984b2c856
David Liebherr <ambition.aidev@gmail.com>
2023-11-05 18:48:02 -0600

    WIP, resume page

--- src/page/Resume.jsx
+++ src/page/Resume.jsx
@@ -1,12 +1,17 @@
 import Header from '../components/Header'
-
+import '../components/Nav/nav.css'
 
 const Resume = () => {
   return (
     <>
         <Header>//reSume</Header>
-        <div className="text-center">
-          <tt>Please check out my LinkIn Page for complete review</tt>
+        <div className='hero'>
+
+        
+        <div className="text-center div_placement">
+          <tt className="tt_bg">click the link <a href='https://profile.indeed.com/p/davidl-f9lyjfh' className='d-flex  line link_style' target='_blank' >// reSume</a> to view and download my full resume from indeed</tt>
+        </div>
+
         </div>
     </>
   )
