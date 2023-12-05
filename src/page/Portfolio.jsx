895e6714db556aa5adef280ae818d864b6f4c9e3
David Liebherr <ambition.aidev@gmail.com>
2023-12-04 18:30:06 -0600

    added target blank to links

--- src/page/Portfolio.jsx
+++ src/page/Portfolio.jsx
@@ -51,20 +51,20 @@ const Portfolio = () => {
             <h2>Some of my previous work!</h2>
          </div>
 
-         <LinkCard><a href="https://www.coreyoil.com/" className="linkDec">Corey Oil</a></LinkCard>
-         <LinkCard><a href="https://www.djctax.com/" className="linkDec">DJC Tax</a></LinkCard>
-         <LinkCard><a href="https://www.aboveaveragemoving.com/" className="linkDec">Above Average Moving</a></LinkCard>
-         <LinkCard><a href="https://www.rockrivercontracting.com/" className="linkDec">Rock River Contracting</a></LinkCard>
-         <LinkCard><a href="https://www.brownhvac.com/" className="linkDec">Brown Hvac</a></LinkCard>
-         <LinkCard><a href="https://www.graylawfirm.info/" className="linkDec">Gray Law</a></LinkCard>
-         <LinkCard><a href="https://www.pribnowsmapleinn.com/" className="linkDec">Maple Inn</a></LinkCard>
-         <LinkCard><a href="https://www.americandoorco.info/" className="linkDec">American Door</a></LinkCard>
-         <LinkCard><a href="https://asdarefuse.com/" className="linkDec">ASDA Refuse</a></LinkCard>
-         <LinkCard><a href="https://www.mmrcnc.com/" className="linkDec">Metro Machines</a></LinkCard>
-         <LinkCard><a href="https://www.mightypac.com/" className="linkDec">Mighty Pac</a></LinkCard>
-         <LinkCard><a href="https://www.wanasek.com/" className="linkDec">Wanasek Corp</a></LinkCard>
-         <LinkCard><a href="https://www.prowiseaccounting.com/" className="linkDec">ProWise Accounting</a></LinkCard>
-         <LinkCard><a href="https://www.freebirdpermanentmakeup.com/" className="linkDec">FreeBird</a></LinkCard>
+         <LinkCard><a href="https://www.coreyoil.com/" className="linkDec" target="_blank">Corey Oil</a></LinkCard>
+         <LinkCard><a href="https://www.djctax.com/" className="linkDec" target="_blank">DJC Tax</a></LinkCard>
+         <LinkCard><a href="https://www.aboveaveragemoving.com/" className="linkDec" target="_blank">Above Average Moving</a></LinkCard>
+         <LinkCard><a href="https://www.rockrivercontracting.com/" className="linkDec" target="_blank">Rock River Contracting</a></LinkCard>
+         <LinkCard><a href="https://www.brownhvac.com/" className="linkDec" target="_blank">Brown Hvac</a></LinkCard>
+         <LinkCard><a href="https://www.graylawfirm.info/" className="linkDec" target="_blank">Gray Law</a></LinkCard>
+         <LinkCard><a href="https://www.pribnowsmapleinn.com/" className="linkDec" target="_blank">Maple Inn</a></LinkCard>
+         <LinkCard><a href="https://www.americandoorco.info/" className="linkDec" target="_blank">American Door</a></LinkCard>
+         <LinkCard><a href="https://asdarefuse.com/" className="linkDec" target="_blank">ASDA Refuse</a></LinkCard>
+         <LinkCard><a href="https://www.mmrcnc.com/" className="linkDec" target="_blank">Metro Machines</a></LinkCard>
+         <LinkCard><a href="https://www.mightypac.com/" className="linkDec" target="_blank">Mighty Pac</a></LinkCard>
+         <LinkCard><a href="https://www.wanasek.com/" className="linkDec" target="_blank">Wanasek Corp</a></LinkCard>
+         <LinkCard><a href="https://www.prowiseaccounting.com/" className="linkDec" target="_blank">ProWise Accounting</a></LinkCard>
+         <LinkCard><a href="https://www.freebirdpermanentmakeup.com/" className="linkDec" target="_blank">FreeBird</a></LinkCard>
 
       </div>
     </>
