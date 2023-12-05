0172e1a2b3caab418cbb3f7c99c1384cdd92a40a
David Liebherr <ambition.aidev@gmail.com>
2023-12-04 18:17:27 -0600

    wip, just a simple, very basic setup for my prevoius woork in portfolio

---
+++ src/components/Cards/linkCards.jsx
@@ -0,0 +1,15 @@
+import { resolveReadonlyArrayThunk } from "graphql"
+
+
+const LinkCard = (props) => {
+
+   return (
+      <>
+         <div className="box">
+            <button className="btnBG">{props.children}</button>
+         </div>
+      </>
+   )
+}
+
+export default LinkCard;
--- src/page/Portfolio.jsx
+++ src/page/Portfolio.jsx
@@ -1,6 +1,7 @@
 import Header from "../components/Header"
 import ProjectCard from "../components/Cards/ProjectCard"
 import ProficiencyCard from "../components/Cards/proficiencyCard"
+import LinkCard from "../components/Cards/linkCards"
 
 
 const Portfolio = () => {
@@ -44,6 +45,28 @@ const Portfolio = () => {
         <ProficiencyCard />
 
       </section>
+
+      <div className="m-5 centerItBaby">
+         <div className="d-flex justify-content-center m-4">
+            <h2>Some of my previous work!</h2>
+         </div>
+
+         <LinkCard><a href="https://www.coreyoil.com/" className="linkDec">Corey Oil</a></LinkCard>
+         <LinkCard><a href="https://www.djctax.com/" className="linkDec">DJC Tax</a></LinkCard>
+         <LinkCard><a href="https://www.aboveaveragemoving.com/" className="linkDec">Above Average Moving</a></LinkCard>
+         <LinkCard><a href="https://www.rockrivercontracting.com/" className="linkDec">Rock River Contracting</a></LinkCard>
+         <LinkCard><a href="https://www.brownhvac.com/" className="linkDec">Brown Hvac</a></LinkCard>
+         <LinkCard><a href="https://www.graylawfirm.info/" className="linkDec">Gray Law</a></LinkCard>
+         <LinkCard><a href="https://www.pribnowsmapleinn.com/" className="linkDec">Maple Inn</a></LinkCard>
+         <LinkCard><a href="https://www.americandoorco.info/" className="linkDec">American Door</a></LinkCard>
+         <LinkCard><a href="https://asdarefuse.com/" className="linkDec">ASDA Refuse</a></LinkCard>
+         <LinkCard><a href="https://www.mmrcnc.com/" className="linkDec">Metro Machines</a></LinkCard>
+         <LinkCard><a href="https://www.mightypac.com/" className="linkDec">Mighty Pac</a></LinkCard>
+         <LinkCard><a href="https://www.wanasek.com/" className="linkDec">Wanasek Corp</a></LinkCard>
+         <LinkCard><a href="https://www.prowiseaccounting.com/" className="linkDec">ProWise Accounting</a></LinkCard>
+         <LinkCard><a href="https://www.freebirdpermanentmakeup.com/" className="linkDec">FreeBird</a></LinkCard>
+
+      </div>
     </>
   )
 }
